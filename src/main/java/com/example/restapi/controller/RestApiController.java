8a17a6d50399b727439a2d3c84724d726830b26b
kangseungmin <rkdtmdals999@naver.com>
2024-01-19 18:34:25 +0900

    [feat] Pass Variable 형태 GetMapping

--- src/main/java/com/example/restapi/controller/RestApiController.java
+++ src/main/java/com/example/restapi/controller/RestApiController.java
@@ -1,6 +1,7 @@
 package com.example.restapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +14,13 @@ public class RestApiController {
         var html = "<html> <body> <h1> Hello Spring Boot </h1> </body> </html>";
         return html;
     }
+
+    @GetMapping(path = "/echo/{message}")
+    public String echo(@PathVariable(name = "message") String msg) {
+        System.out.println("echo message = " + msg);
+        // TODO: 대문자로 변환해서 리턴
+        String result = msg.toUpperCase();
+        return result;
+    }
+
 }
